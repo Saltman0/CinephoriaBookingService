19355f1fd57f645ab1a525a8e628a79aeeb36e45
Saltman0 <baudoin.mathieu@protonmail.com>
2025-01-23 10:55:52 +0100

    Update booking.routes.ts

--- src/routes/booking.routes.ts
+++ src/routes/booking.routes.ts
@@ -4,7 +4,7 @@ import passport from "../middleware/passport";
 
 const router: Router = Router();
 
-router.get("/booking", passport.authenticate("jwt", { session: false }), bookingController.getBookings);
+router.get("/booking/:userId", /*passport.authenticate("jwt", { session: false }),*/ bookingController.getBookings);
 router.get("/booking/:id", passport.authenticate("jwt", { session: false }), bookingController.getBookingById);
 router.post("/booking", passport.authenticate("jwt", { session: false }), bookingController.createBooking);
 router.put("/booking/:id", passport.authenticate("jwt", { session: false }), bookingController.updateBooking);
