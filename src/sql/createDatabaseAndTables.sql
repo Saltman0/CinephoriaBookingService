bfb77c65d9cfc855e82c9283f0495e3fc6fe00ca
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-08 11:03:46 +0200

    :art: SQL files updated

--- src/sql/createDatabaseAndTables.sql
+++ src/sql/createDatabaseAndTables.sql
@@ -1,7 +1,7 @@
--- Cinéphoria movie service database
+-- Cinéphoria booking service database
 DROP DATABASE IF EXISTS "cinephoriaBookingServiceDatabase";
 CREATE DATABASE "cinephoriaBookingServiceDatabase";
--- Cinéphoria movie service database
+-- Cinéphoria booking service database
 
 -- Booking table
 DROP TABLE IF EXISTS "booking";
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS "booking"
 (
     "id" SERIAL PRIMARY KEY,
     "qrCode" TEXT NOT NULL,
-    "userId" INTEGER REFERENCES "user"(id) ON UPDATE NO ACTION ON DELETE CASCADE,
-    "showtimeId" INTEGER REFERENCES "showtime"(id) ON UPDATE NO ACTION ON DELETE CASCADE
+    "userId" INTEGER NOT NULL,
+    "showtimeId" INTEGER NOT NULL
 );
 -- Booking table
 
@@ -19,7 +19,7 @@ DROP TABLE IF EXISTS "bookingSeat";
 CREATE TABLE IF NOT EXISTS "bookingSeat"
 (
     "id" SERIAL PRIMARY KEY,
-    "bookingId" INTEGER REFERENCES "booking"(id) ON UPDATE NO ACTION ON DELETE CASCADE,
-    "seatId" INTEGER REFERENCES "seat"(id) ON UPDATE NO ACTION ON DELETE CASCADE
+    "bookingId" INTEGER NOT NULL,
+    "seatId" INTEGER NOT NULL
 );
 -- BookingSeat table
--- src/sql/insertFirstData.sql
+++ src/sql/insertFirstData.sql
@@ -1,7 +1,45 @@
 -- Start the SQL transaction
 BEGIN;
 
--- TODO Rajouter des INSERT INTO pour Booking et BookingSeat
+-- Insert booking
+INSERT INTO "booking" ("qrCode", "userId", "showtimeId")
+VALUES ('2025-06-07 18:00:00', 1, 1),
+       ('2025-06-07 18:00:00', 2, 1),
+       ('2025-06-07 18:00:00', 3, 1),
+       ('2025-06-07 18:00:00', 4, 1),
+       ('2025-06-07 18:00:00', 5, 1),
+       ('2025-06-07 18:00:00', 1, 2),
+       ('2025-06-07 18:00:00', 2, 2),
+       ('2025-06-07 18:00:00', 3, 2),
+       ('2025-06-07 18:00:00', 4, 2),
+       ('2025-06-07 18:00:00', 5, 2);
+-- Insert booking
+
+SAVEPOINT booking_savepoint;
+
+-- Insert bookingSeat
+INSERT INTO "bookingSeat" ("bookingId", "seatId")
+VALUES (1, 1),
+       (1, 2),
+       (2, 7),
+       (2, 8),
+       (2, 9),
+       (3, 16),
+       (4, 17),
+       (4, 18),
+       (4, 19),
+       (5, 20),
+       (6, 23),
+       (6, 24),
+       (7, 26),
+       (7, 27),
+       (8, 31),
+       (9, 35),
+       (9, 36),
+       (10, 40);
+-- Insert bookingSeat
+
+SAVEPOINT bookingSeat_savepoint;
 
 -- Commit if successful
 COMMIT;
