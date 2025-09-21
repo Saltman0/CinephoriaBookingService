313fde0563239dc30cdaf2ee4691b62e60f23359
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-21 17:16:53 +0200

    ✨ CreateTables SQL added

---
+++ src/sql/createTables.sql
@@ -0,0 +1,19 @@
+-- Booking table
+DROP TABLE IF EXISTS "booking";
+CREATE TABLE IF NOT EXISTS "booking"
+(
+    "id" SERIAL PRIMARY KEY,
+    "userId" INTEGER NOT NULL,
+    "showtimeId" INTEGER NOT NULL
+);
+-- Booking table
+
+-- BookingSeat table
+DROP TABLE IF EXISTS "bookingSeat";
+CREATE TABLE IF NOT EXISTS "bookingSeat"
+(
+    "id" SERIAL PRIMARY KEY,
+    "bookingId" INTEGER NOT NULL,
+    "seatId" INTEGER NOT NULL
+);
+-- BookingSeat table
