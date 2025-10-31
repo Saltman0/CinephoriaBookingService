55f4e03b19fc47aaa5adbb0900ee7892dc107691
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-31 01:18:17 +0100

    ♻️ Payment controller updated

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -5,27 +5,13 @@ export async function createPayment(req: Request, res: Response) {
     try {
         const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
-        const paymentLink = await stripe.paymentLinks.create({
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'eur',
-                        product_data: {
-                            name: 'Réservation des sièges'
-                        },
-                        unit_amount: req.body.price
-                    },
-                    quantity: 1
-                },
-            ],
-            payment_method_types: ['card'],
-            after_completion: {
-                type: "redirect",
-                redirect: { url: "http://localhost:4200/home" }
-            }
+        const paymentLink = await stripe.paymentIntents.create({
+            amount: req.body.price,
+            currency: "eur",
+            automatic_payment_methods: { enabled: true }
         });
 
-        res.status(200).json(paymentLink.url);
+        res.status(200).json({ clientSecret: paymentLink.client_secret });
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).json({ message: error.message });
