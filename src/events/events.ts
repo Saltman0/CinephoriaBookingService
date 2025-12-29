c9909113686d0c25b4638527a027946c4fbaf2cd
Saltman0 <baudoin.mathieu@protonmail.com>
2025-12-30 00:47:17 +0100

    ✨ DeleteByShowtimeBooking event added

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,12 +1,14 @@
 import EventEmitter from "node:events";
-import * as cinemaEvents from "./seat.events";
+import * as seatEvents from "./seat.events";
+import * as bookingEvents from "./booking.events";
 
 export const eventEmitter: EventEmitter = new EventEmitter();
 
 eventEmitter.on("messageReceived", async (message: { type: string, event: string, body: any }): Promise<void> => {
     try {
         const actions: { [key: string]: (message: any) => Promise<void> } = {
-            "deleteSeat": cinemaEvents.deleteSeatEvent
+            "deleteSeat": seatEvents.deleteSeatEvent,
+            "deleteByShowtimeBooking": bookingEvents.deleteBookingByShowtimeId
         };
 
         const key: string = `${message.event}${message.type.charAt(0).toUpperCase() + message.type.slice(1)}`;
